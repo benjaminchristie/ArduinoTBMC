b7fc854385bd22f5924c4b0f6782df9d0df79511
Ben Christie <benc00@vt.edu>
2020-12-07 20:16:28 -0500

    added hw4 configuration to temperatureCode file, also added Serial Plotter capabilities

--- temperatureCode/temperatureCode.ino
+++ temperatureCode/temperatureCode.ino
@@ -1,25 +1,21 @@
 // verify compilation using the verify/compile tool
 #include <Adafruit_MAX31865.h> // be sure to have the Adafruit_MAX31865 and Adafruit_BusIO libraries installed.
 const int enablePin = 11;  // adjust all pin values need be
-const int in1Pin = 10;
-const int in2Pin = 9;
 const int tempPin = 12;
 const float RREF = 4300.0;
 Adafruit_MAX31865 max = Adafruit_MAX31865(4, 5, 6, 7); // adjust pin values need be
-float currentTemp = 0;
-float idealTemp = 450;
+float currTemp = 0;
+float idealTemp = 350;
 
 void setup() {                 // (2)
   pinMode(enablePin, OUTPUT);
-  pinMode(in1Pin, OUTPUT);
-  pinMode(in2Pin, OUTPUT);
   Serial.begin(9600);
   // Use software SPI: CS, DI, DO, CLK
   max.begin(MAX31865_4WIRE);
   if (testFailure()) {
     noInterrupts();
-    while(1) {} // continue to loop and do nothing so that there is not a hardware issue with
-  } 
+    while (1) {} // continue to loop and do nothing so that there is not a hardware issue with
+  }
 }
 
 bool testFailure() {
@@ -54,37 +50,30 @@ float getTemp() {
   float temp = max.temperature(1000, RREF); // 1000 because PT1000
   return temp;
 }
+
 void loop() {                         // (3)
-  float currTemp = getTemp();
-  // note that this is not proper thermodynamics, 
-  // but i am doing a computation where Q ranges from 100%-0% based on 0-350degC inversely proportional
-  float rate = 255.0*(((idealTemp - currTemp)/idealTemp));
-  forward(rate);
-  delay(10);
+  currTemp = getTemp();
+  if (currTemp >= idealTemp) {
+    stop();
+  }
+  else {
+    // note that this is not proper thermodynamics,
+    // but i am doing a computation where Q ranges from 100%-0% based on 0-350degC inversely proportional
+    float rate = 255.0 * (((idealTemp - currTemp) / idealTemp));
+    forward(rate);
+    Serial.print(rate);
+    Serial.print(",");
+    Serial.println(currTemp);
+    delay(20);
+  }
 }
 
 void forward(int pwm)          // (9)
 {
-  digitalWrite(in1Pin, HIGH);
-  digitalWrite(in2Pin, LOW);
   analogWrite(enablePin, pwm);
-  Serial.print("Forward ");
-  Serial.println(pwm);
-}
-
-void reverse(int pwm)          // (10)
-{
-  digitalWrite(in1Pin, LOW);
-  digitalWrite(in2Pin, HIGH);
-  analogWrite(enablePin, pwm);
-  Serial.print("Reverse ");
-  Serial.println(pwm);
 }
 
 void stop()                    // (11)
 {
-  digitalWrite(in1Pin, LOW);
-  digitalWrite(in2Pin, LOW);
   analogWrite(enablePin, 0);
-  Serial.println("Stop");
 }
