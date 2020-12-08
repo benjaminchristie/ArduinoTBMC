7736f16ddbdb8ccbc3229d8c498c7ed06c9cd862
Ben Christie <benc00@vt.edu>
2020-12-07 20:08:09 -0500

    fixed changes to pwm to allow for simpler control for demo

--- temperatureCodeWithoutTempSensor/temperatureCodeWithoutTempSensor.ino
+++ temperatureCodeWithoutTempSensor/temperatureCodeWithoutTempSensor.ino
@@ -1,16 +1,11 @@
 // verify compilation using the verify/compile tool
 const int enablePin = 11;  // adjust all pin values need be
-const int in1Pin = 10;
-const int in2Pin = 9;
-const int tempPin = 12;
 float currentTemp = 0;
 float idealTemp = 350;
 float iterator = 0;
 
 void setup() {                 // (2)
   pinMode(enablePin, OUTPUT);
-  pinMode(in1Pin, OUTPUT);
-  pinMode(in2Pin, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -24,9 +19,8 @@ void loop() {                         // (3)
     float rate = 255.0*(((idealTemp - currentTemp)/idealTemp));
     forward(rate);
     delay(20);
-    Serial.print("Rate ");
-    Serial.println(rate);
-    Serial.print("Current Temp ");
+    Serial.print(rate);
+    Serial.print(",");
     Serial.println(currentTemp);
     currentTemp = currentTemp + 1;
   }
@@ -34,17 +28,10 @@ void loop() {                         // (3)
 
 void forward(int pwm)          // (9)
 {
-  digitalWrite(in1Pin, HIGH);
-  digitalWrite(in2Pin, LOW);
   analogWrite(enablePin, pwm);
-  Serial.print("Forward ");
-  Serial.println(pwm);
 }
 
 void stop()                    // (11)
 {
-  digitalWrite(in1Pin, LOW);
-  digitalWrite(in2Pin, LOW);
   analogWrite(enablePin, 0);
-  Serial.println("Stop");
 }
